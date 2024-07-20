b15ff9de907d4e0bccfb05d777a5bf1dc7e1a925
Kamil Jach <kamiljach2004@gmail.com>
2024-07-20 13:27:29 +0200

    Clean project

---
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/DevJobsHubApplication.java
@@ -0,0 +1,13 @@
+package com.kamiljach.devjobshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJobsHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevJobsHubApplication.class, args);
+	}
+
+}
---
+++ devjobshub-backend/src/test/java/com/kamiljach/devjobshub/DevJobsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamiljach.devjobshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJobsHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
