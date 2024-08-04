3a6ecb7638be3ebfa065a79f7c2c4a6e53b03092
Kamil Jach <kamiljach2004@gmail.com>
2024-08-04 14:15:14 +0200

    RegisterRequest added

---
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/request/register/RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.kamiljach.devjobshub.request.register;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+}
