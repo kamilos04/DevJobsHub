0e1e57110adfbb1c1f83669f0c1fd15d7d314ed9
Kamil Jach <kamiljach2004@gmail.com>
2024-08-04 14:14:14 +0200

    Date formate changed in ApiError

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/errors/ApiError.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/errors/ApiError.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 
 public class ApiError {
     private HttpStatus status;
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime timestamp;
     private String message;
     private String debugMessage;
