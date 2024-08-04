9e6fb8f93732714aff25d2cbbdcd500546dcd2f0
Kamil Jach <kamiljach2004@gmail.com>
2024-08-04 21:32:28 +0200

    JobLevel and OperatingMode enums added

---
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/model/OFFER_JOB_LEVEL.java
@@ -0,0 +1,10 @@
+package com.kamiljach.devjobshub.model;
+
+public enum OFFER_JOB_LEVEL {
+    TRAINEE,
+    JUNIOR,
+    MID,
+    SENIOR,
+    MANAGER,
+    CLEVEL
+}
---
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/model/OFFER_OPERATING_MODE.java
@@ -0,0 +1,7 @@
+package com.kamiljach.devjobshub.model;
+
+public enum OFFER_OPERATING_MODE {
+    REMOTE,
+    HYBRID,
+    STATIONARY
+}
