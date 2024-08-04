081858422c5c5d8af2654ba185d83e92de1a1077
Kamil Jach <kamiljach2004@gmail.com>
2024-08-04 21:58:46 +0200

    List of cities added

---
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/config/Constants.java
@@ -0,0 +1,26 @@
+package com.kamiljach.devjobshub.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constants {
+    public static String secretKey = "shildgopsohpgqwbjkqwrqhjkfdsjkfjkewf";
+    public static long expirationTime = 3600000;
+
+    public static String header = "Authorization";
+
+    public static String prefix = "Bearer ";
+
+    public List<String> cities = List.of(
+            "Warszawa", "Kraków", "Łódź", "Wrocław", "Poznań",
+            "Gdańsk", "Szczecin", "Bydgoszcz", "Lublin", "Białystok",
+            "Katowice", "Gdynia", "Czestochowa", "Radom", "Sosnowiec",
+            "Tychy", "Gliwice", "Zabrze", "Ruda Śląska", "Rybnik",
+            "Olsztyn", "Opole", "Elbląg", "Toruń", "Jaworzno",
+            "Płock", "Dąbrowa Górnicza", "Słupsk", "Zielona Góra", "Kalisz",
+            "Koszalin", "Mysłowice", "Gorzów Wielkopolski", "Siedlce", "Legnica",
+            "Grudziądz", "Świętochłowice", "Nowy Sącz", "Tarnobrzeg", "Bielsko-Biała",
+            "Stalowa Wola", "Pabianice", "Rybnik", "Zamość", "Jelenia Góra",
+            "Hajnówka", "Łomża", "Włocławek"
+    );
+}
