2ab6192318662db1c948a61bdca4ec65c0d6863d
Kamil Jach <kamiljach2004@gmail.com>
2024-08-17 12:28:39 +0200

    Enum specializations created

---
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/model/SPECIALIZATIONS.java
@@ -0,0 +1,18 @@
+package com.kamiljach.devjobshub.model;
+
+public enum SPECIALIZATIONS {
+    BACKEND,
+    FRONTEND,
+    FULLSTACK,
+    MOBILE,
+    EMBEDDED,
+    ARCHITECTURE,
+    TESTING,
+    SECURITY,
+    AIML,
+    DATASCIENCE,
+    HELPDESK,
+    UXUI,
+    ADMINISTRATOR,
+    PROJECTMANAGER
+}
