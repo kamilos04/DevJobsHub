7cc985cc18b1bee2567af9ee7f6073ec44086d4a
Kamil Jach <kamiljach2004@gmail.com>
2024-09-01 01:33:54 +0200

    addCandidate(), addLikedByUser() etc. created in OfferService

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/OfferService.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/OfferService.java
@@ -3,6 +3,7 @@ package com.kamiljach.devjobshub.service;
 import com.kamiljach.devjobshub.exceptions.OfferNotFoundByIdException;
 import com.kamiljach.devjobshub.model.Offer;
 import com.kamiljach.devjobshub.model.Technology;
+import com.kamiljach.devjobshub.model.User;
 
 import java.util.ArrayList;
 
@@ -12,4 +13,9 @@ public interface OfferService {
 //    public void addTechnologyToRequiredTechnologies(Offer offer , Technology technology);
 //
 //    public void addTechnologyToNiceToHaveTechnologies(Offer offer, Technology technology);
+
+    public void addCandidate(Offer offer, User user);
+    public void addLikedByUser(Offer offer, User user);
+    public void addRequiredTechnology(Offer offer, Technology technology);
+    public void addNiceToHaveTechnology(Offer offer, Technology technology);
 }
--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/impl/OfferServiceImpl.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/impl/OfferServiceImpl.java
@@ -1,11 +1,19 @@
 package com.kamiljach.devjobshub.service.impl;
 
 
+import com.kamiljach.devjobshub.dto.OfferDto;
+import com.kamiljach.devjobshub.mappers.OfferMapper;
+import com.kamiljach.devjobshub.model.Technology;
+import com.kamiljach.devjobshub.model.User;
+import com.kamiljach.devjobshub.repository.TechnologyRepository;
+import com.kamiljach.devjobshub.repository.UserRepository;
+import com.kamiljach.devjobshub.request.offer.CreateOfferRequest;
 import com.kamiljach.devjobshub.service.UtilityClass;
 import com.kamiljach.devjobshub.exceptions.OfferNotFoundByIdException;
 import com.kamiljach.devjobshub.model.Offer;
 import com.kamiljach.devjobshub.repository.OfferRepository;
 import com.kamiljach.devjobshub.service.OfferService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,9 +22,14 @@ import java.util.Optional;
 @Service
 public class OfferServiceImpl implements OfferService {
     private OfferRepository offerRepository;
+    private UserRepository userRepository;
+    private TechnologyRepository technologyRepository;
 
-    public OfferServiceImpl(OfferRepository offerRepository) {
+
+    public OfferServiceImpl(OfferRepository offerRepository, UserRepository userRepository, TechnologyRepository technologyRepository) {
         this.offerRepository = offerRepository;
+        this.userRepository = userRepository;
+        this.technologyRepository = technologyRepository;
     }
 
     public ArrayList<Offer> getListOfOffersFromTheirIds(ArrayList<Long> list) throws OfferNotFoundByIdException {
@@ -35,13 +48,38 @@ public class OfferServiceImpl implements OfferService {
         return offerList;
     }
 
-//    public void addTechnologyToRequiredTechnologies(Offer offer ,Technology technology){
-//        offer.getRequiredTechnologies().add(technology);
-//        offerRepository.save(offer);
-//    }
+
+//    public OfferDto createOffer(CreateOfferRequest createOfferRequest, String jwt){
+//        Offer newOffer = OfferMapper.INSTANCE.createOfferRequestToOffer(createOfferRequest);
 //
-//    public void addTechnologyToNiceToHaveTechnologies(Offer offer, Technology technology){
-//        offer.getNiceToHaveTechnologies().add(technology);
-//        offerRepository.save(offer);
 //    }
+
+    @Transactional
+    public void addCandidate(Offer offer, User user){
+        offer.addCandidate(user);
+        offerRepository.save(offer);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void addLikedByUser(Offer offer, User user){
+        offer.addLikedByUser(user);
+        offerRepository.save(offer);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void addRequiredTechnology(Offer offer, Technology technology){
+        offer.addRequiredTechnology(technology);
+        offerRepository.save(offer);
+        technologyRepository.save(technology);
+    }
+
+    @Transactional
+    public void addNiceToHaveTechnology(Offer offer, Technology technology){
+        offer.addNiceToHaveTechnology(technology);
+        offerRepository.save(offer);
+        technologyRepository.save(technology);
+    }
+
 }
