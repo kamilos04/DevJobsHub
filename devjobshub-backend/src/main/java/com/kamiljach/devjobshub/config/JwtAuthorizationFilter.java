08aa1a18ead09f6dd51ec52af07abf609ef50fdf
Kamil Jach <kamiljach2004@gmail.com>
2024-09-01 13:04:22 +0200

    JwtAuthorizationFIlter - added verification whether user exists

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/config/JwtAuthorizationFilter.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/config/JwtAuthorizationFilter.java
@@ -1,6 +1,9 @@
 package com.kamiljach.devjobshub.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kamiljach.devjobshub.exceptions.UserNotFoundByJwtException;
+import com.kamiljach.devjobshub.model.User;
+import com.kamiljach.devjobshub.repository.UserRepository;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -19,15 +22,19 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
     private JwtConfig jwtConfig;
     private ObjectMapper mapper;
 
-    public JwtAuthorizationFilter(JwtConfig jwtConfig, ObjectMapper mapper) {
+    private UserRepository userRepository;
+
+    public JwtAuthorizationFilter(JwtConfig jwtConfig, ObjectMapper mapper, UserRepository userRepository) {
         this.jwtConfig = jwtConfig;
         this.mapper = mapper;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -46,6 +53,10 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             if(claims!=null && jwtConfig.validateClaims(claims)){
                 String email = claims.getSubject();
                 System.out.println("email : "+email);
+                Optional<User> optionalUser = userRepository.findByEmail(email);
+                if(optionalUser.isEmpty()){
+                    throw new UserNotFoundByJwtException();
+                }
                 Authentication authentication = new UsernamePasswordAuthenticationToken(email, "", new ArrayList<>());
                 SecurityContextHolder.getContext().setAuthentication(authentication);
                 filterChain.doFilter(request, response);
