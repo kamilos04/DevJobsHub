cacc0ebe0062d319084e7003c2471f35c0c4a538
Kamil Jach <kamiljach2004@gmail.com>
2024-09-22 23:02:33 +0200

    updateOffer endpoint created

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/controller/OfferController.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/controller/OfferController.java
@@ -1,16 +1,14 @@
 package com.kamiljach.devjobshub.controller;
 
 import com.kamiljach.devjobshub.dto.OfferDto;
+import com.kamiljach.devjobshub.exceptions.exceptions.OfferNotFoundByIdException;
 import com.kamiljach.devjobshub.exceptions.exceptions.TechnologyNotFoundByIdException;
 import com.kamiljach.devjobshub.request.offer.CreateOfferRequest;
 import com.kamiljach.devjobshub.service.OfferService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api")
@@ -27,5 +25,11 @@ public class OfferController {
     public ResponseEntity<OfferDto> createOffer(@RequestBody CreateOfferRequest createOfferRequest, @RequestHeader("Authorization")String jwt) throws TechnologyNotFoundByIdException {
         OfferDto newOfferDto = offerService.createOffer(createOfferRequest, jwt);
         return new ResponseEntity<>(newOfferDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/offer/{offerId}")
+    public ResponseEntity<OfferDto> updateOffer(@RequestBody CreateOfferRequest createOfferRequest, @PathVariable("offerId") Long offerId, @RequestHeader("Authorization")String jwt) throws OfferNotFoundByIdException, TechnologyNotFoundByIdException {
+        OfferDto updatedOfferDto = offerService.updateOffer(createOfferRequest, offerId, jwt);
+        return new ResponseEntity<>(updatedOfferDto, HttpStatus.OK);
     }
 }
