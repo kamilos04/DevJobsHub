7db4638a3d09d88334d677b1417f2002dc765e23
Kamil Jach <kamiljach2004@gmail.com>
2024-10-19 18:43:20 +0200

    questions validating added

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/impl/ApplicationServiceImpl.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/impl/ApplicationServiceImpl.java
@@ -3,11 +3,13 @@ package com.kamiljach.devjobshub.service.impl;
 import com.kamiljach.devjobshub.dto.ApplicationDto;
 import com.kamiljach.devjobshub.exceptions.exceptions.ApplicationNotFoundByIdException;
 import com.kamiljach.devjobshub.exceptions.exceptions.OfferNotFoundByIdException;
+import com.kamiljach.devjobshub.exceptions.exceptions.QuestionOrAnswerIsIncorrectException;
 import com.kamiljach.devjobshub.exceptions.exceptions.UserNotFoundByJwtException;
 import com.kamiljach.devjobshub.mappers.ApplicationMapper;
 import com.kamiljach.devjobshub.model.Application;
 import com.kamiljach.devjobshub.model.Offer;
 import com.kamiljach.devjobshub.model.User;
+import com.kamiljach.devjobshub.model.embeddable.*;
 import com.kamiljach.devjobshub.repository.ApplicationRepository;
 import com.kamiljach.devjobshub.repository.OfferRepository;
 import com.kamiljach.devjobshub.repository.UserRepository;
@@ -36,10 +38,13 @@ public class ApplicationServiceImpl implements ApplicationService {
     }
 
     @Transactional(rollbackOn = Exception.class)
-    public ApplicationDto applyForOffer(CreateApplicationRequest createApplicationRequest, Long offerId, String jwt) throws OfferNotFoundByIdException, UserNotFoundByJwtException {
+    public ApplicationDto applyForOffer(CreateApplicationRequest createApplicationRequest, Long offerId, String jwt) throws OfferNotFoundByIdException, UserNotFoundByJwtException, QuestionOrAnswerIsIncorrectException {
         Optional<Offer> optionalOffer = offerRepository.findById(offerId);
         if(optionalOffer.isEmpty()){throw new OfferNotFoundByIdException();}
         Application newApplication = ApplicationMapper.INSTANCE.createApplicationRequestToApplication(createApplicationRequest);
+
+        validateAllQuestionsInApplication(newApplication, optionalOffer.get());
+
         setUserInApplication(newApplication, userService.findUserByJwt(jwt));
         setOfferInApplication(newApplication, optionalOffer.get());
         applicationRepository.save(newApplication);
@@ -91,5 +96,71 @@ public class ApplicationServiceImpl implements ApplicationService {
         application.deleteUser();
         userRepository.save(user);
         applicationRepository.save(application);
+    }
+
+    public boolean validateQuestion(QuestionAndAnswer questionAndAnswer, Question question){
+        if(!questionAndAnswer.getNumber().equals(question.getNumber())){
+            return(false);
+        }
+        if(!questionAndAnswer.getQuestion().equals(question.getQuestion())){
+            return(false);
+        }
+        return(true);
+
+    }
+
+    public boolean validateRadioQuestion(RadioQuestionAndAnswer radioQuestionAndAnswer, RadioQuestion radioQuestion){
+        if(!radioQuestionAndAnswer.getNumber().equals(radioQuestion.getNumber())){
+            return false;
+        }
+        if(!radioQuestionAndAnswer.getQuestion().equals(radioQuestion.getQuestion())){
+            return false;
+        }
+
+        if(!(radioQuestionAndAnswer.getAnswer()>=0 && radioQuestionAndAnswer.getAnswer()<radioQuestion.getPossibleAnswers().size())){
+            return false;
+        }
+        return(true);
+
+    }
+
+    public boolean validateMultipleChoiceQuestion(MultipleChoiceQuestionAndAnswer multipleChoiceQuestionAndAnswer, MultipleChoiceQuestion multipleChoiceQuestion){
+        if(!multipleChoiceQuestionAndAnswer.getNumber().equals(multipleChoiceQuestion.getNumber())){
+            return(false);
+        }
+        if(!multipleChoiceQuestionAndAnswer.getQuestion().equals(multipleChoiceQuestion.getQuestion())){
+            return(false);
+        }
+        for(int i = 0; i < multipleChoiceQuestionAndAnswer.getAnswers().size(); i++){
+            System.out.println(multipleChoiceQuestionAndAnswer.getAnswers().size());
+            Integer answer = multipleChoiceQuestionAndAnswer.getAnswers().get(i);
+            if(!(answer>=0 && answer < multipleChoiceQuestion.getPossibleAnswers().size())){
+                return false;
+            }
+        }
+        return(true);
+
+    }
+
+    public void validateAllQuestionsInApplication(Application application, Offer offer) throws QuestionOrAnswerIsIncorrectException {
+        for(int i = 0; i < application.getQuestionsAndAnswers().size(); i++){
+            if(!validateQuestion(application.getQuestionsAndAnswers().get(i), offer.getQuestions().get(i))){
+                throw new QuestionOrAnswerIsIncorrectException();
+            }
+        }
+
+        for(int i = 0; i < application.getRadioQuestionsAndAnswers().size(); i++){
+            if(!validateRadioQuestion(application.getRadioQuestionsAndAnswers().get(i), offer.getRadioQuestions().get(i))){
+                throw new QuestionOrAnswerIsIncorrectException();
+            }
+        }
+
+        for(int i = 0; i < application.getMultipleChoiceQuestionsAndAnswers().size(); i++){
+            if(!validateMultipleChoiceQuestion(application.getMultipleChoiceQuestionsAndAnswers().get(i), offer.getMultipleChoiceQuestions().get(i))){
+                throw new QuestionOrAnswerIsIncorrectException();
+            }
+        }
+
+
     }
 }
