852b00b25662296fafcaeadffd8d91bdabebe260
Kamil Jach <kamiljach2004@gmail.com>
2024-10-23 18:21:05 +0200

    deleteTechnologyById() function unit test created

--- devjobshub-backend/src/test/java/com/kamiljach/devjobshub/service/TechnologyServiceImplTests.java
+++ devjobshub-backend/src/test/java/com/kamiljach/devjobshub/service/TechnologyServiceImplTests.java
@@ -5,8 +5,11 @@ import static org.mockito.Mockito.*;
 
 import com.kamiljach.devjobshub.dto.TechnologyDto;
 import com.kamiljach.devjobshub.exceptions.exceptions.OfferNotFoundByIdException;
+import com.kamiljach.devjobshub.exceptions.exceptions.TechnologyNotFoundByIdException;
 import com.kamiljach.devjobshub.exceptions.exceptions.TechnologyWithThisNameAlreadyExistsException;
+import com.kamiljach.devjobshub.model.Offer;
 import com.kamiljach.devjobshub.model.Technology;
+import com.kamiljach.devjobshub.repository.OfferRepository;
 import com.kamiljach.devjobshub.repository.TechnologyRepository;
 import com.kamiljach.devjobshub.request.technology.CreateTechnologyRequest;
 import com.kamiljach.devjobshub.service.impl.TechnologyServiceImpl;
@@ -19,12 +22,15 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
+import java.util.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TechnologyServiceImplTests {
     @Mock
     private TechnologyRepository technologyRepository;
+
+    @Mock
+    private OfferRepository offerRepository;
 
 
     private CreateTechnologyRequest validRequests;
@@ -61,5 +67,33 @@ public class TechnologyServiceImplTests {
             technologyService.createTechnology(validRequests, "some jwt");}
         );
         verify(technologyRepository, never()).save(any(Technology.class));
+    }
+
+    @Test
+    public void TechnologyService_deleteTechnologyById_Success() throws TechnologyNotFoundByIdException {
+        TechnologyServiceImpl technologyServiceSpy = Mockito.spy(technologyService);
+
+        Long validId = 1L;
+        String validJwt = "some jwt";
+        Technology technologyToDelete = new Technology();
+        technologyToDelete.setId(validId);
+
+        Offer offer1 = mock(Offer.class);
+        Offer offer2 = mock(Offer.class);
+        Offer offer3 = mock(Offer.class);
+
+        technologyToDelete.setAssignedAsNiceToHave(new ArrayList<>(List.of(offer1, offer2)));
+        technologyToDelete.setAssignedAsRequired(new ArrayList<>(List.of(offer1, offer2, offer3)));
+
+
+        when(technologyRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.of(technologyToDelete));
+
+
+        technologyServiceSpy.deleteTechnologyById(validId, "some jwt");
+
+
+        verify(technologyRepository, times(1)).delete(Mockito.any(Technology.class));
+        verify(technologyServiceSpy, times(2)).deleteOfferFromAssignedAsNiceToHave(eq(technologyToDelete), Mockito.any(Offer.class));
+        verify(technologyServiceSpy, times(3)).deleteOfferFromAssignedAsRequired(eq(technologyToDelete), Mockito.any(Offer.class));
     }
 }
