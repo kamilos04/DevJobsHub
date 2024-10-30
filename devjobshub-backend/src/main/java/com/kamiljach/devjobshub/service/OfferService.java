5d7187f802a07bd76d900d21640d0c12160c6cd9
Kamil Jach <kamiljach2004@gmail.com>
2024-10-30 17:32:31 +0100

    searchOffer() in OfferService created

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/OfferService.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/OfferService.java
@@ -7,6 +7,9 @@ import com.kamiljach.devjobshub.model.Offer;
 import com.kamiljach.devjobshub.model.Technology;
 import com.kamiljach.devjobshub.model.User;
 import com.kamiljach.devjobshub.request.offer.CreateOfferRequest;
+import com.kamiljach.devjobshub.request.offer.SearchOffersRequest;
+
+import java.util.ArrayList;
 
 public interface OfferService {
 //    public ArrayList<Offer> getListOfOffersFromTheirIds(ArrayList<Long> list) throws OfferNotFoundByIdException ;
@@ -25,4 +28,6 @@ public interface OfferService {
     public OfferDto updateOffer(CreateOfferRequest createOfferRequest, Long offerId, String jwt) throws OfferNotFoundByIdException, TechnologyNotFoundByIdException;
 
     public OfferDto getOffer(Long offerId, String jwt) throws OfferNotFoundByIdException;
+
+    public ArrayList<OfferDto> searchOffer(SearchOffersRequest searchOffersRequest, String jwt);
 }
