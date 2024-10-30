c8d2ed57ffd25efc66570ece88641045a6171ab7
Kamil Jach <kamiljach2004@gmail.com>
2024-10-30 17:31:35 +0100

    Query for searchOffer()

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/repository/OfferRepository.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/repository/OfferRepository.java
@@ -1,11 +1,27 @@
 package com.kamiljach.devjobshub.repository;
 
 import com.kamiljach.devjobshub.model.Offer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
+    @Query("SELECT o FROM Offer o WHERE LOWER(o.name) LIKE LOWER(CONCAT('%', :text, '%')) " +
+            "AND o.jobLevel IN :jobLevels " +
+            "AND o.operatingMode IN :operatingModes " +
+            "AND o.localization IN :localizations ")
+    Page<Offer> searchOffers(@Param("text") String text, @Param("jobLevels") List<String> jobLevels, @Param("operatingModes") List<String> operatingModes, @Param("localizations") List<String> localizations, Pageable pageable);
+
+//    @Query("SELECT o FROM Offer o WHERE o.name LIKE CONCAT('%', :text, '%') " )
+////            "AND o.jobLevel IN :jobLevels " +
+////            "AND o.operatingMode IN :operatingModes " +
+////            "AND o.localization IN :localizations ")
+//    Page<Offer> searchOffers(@Param("text") String text, Pageable pageable);
 }
