8443fc2aa7238f69c88854106cb140594eee5fd9
Kamil Jach <kamiljach2004@gmail.com>
2024-11-26 21:52:20 +0100

    small bug fix

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/UtilityService.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/service/UtilityService.java
@@ -13,5 +13,9 @@ public interface UtilityService {
     public ArrayList<Technology> getListOfTechnologiesFromTheirIds(ArrayList<Long> list) throws TechnologyNotFoundByIdException;
     public ArrayList<Offer> getListOfOffersFromTheirIds(ArrayList<Long> list) throws OfferNotFoundByIdException;
 
+    public void removeOfferFromApplication(Application application);
+
+    public void removeUserFromApplication(Application application);
+
     public void deleteApplication(Application application);
 }
