a3a4eb7fdef4300350e0387b0eb66cf0ee743696
Kamil Jach <kamiljach2004@gmail.com>
2024-12-03 16:07:29 +0100

    Auth requests properties validation

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/request/login/LoginRequest.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/request/login/LoginRequest.java
@@ -1,5 +1,7 @@
 package com.kamiljach.devjobshub.request.login;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
+    @Size(min = 1, max = 100, message = "email must be between 1 and 100 characters")
+    @NotBlank(message = "email can not be blank")
     private String email;
+
+    @Size(min = 1, max = 100, message = "password must be between 1 and 100 characters")
+    @NotBlank(message = "password can not be blank")
     private String password;
 }
--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/request/register/RegisterRequest.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/request/register/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.kamiljach.devjobshub.request.register;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @Size(min = 1, max = 100, message = "email must be between 1 and 100 characters")
+    @NotBlank(message = "email can not be blank")
     private String email;
+
+    @Size(min = 1, max = 100, message = "password must be between 1 and 100 characters")
+    @NotBlank(message = "password can not be blank")
     private String password;
+
+    @Size(min = 1, max = 100, message = "name must be between 1 and 100 characters")
+    @NotBlank(message = "name can not be blank")
     private String name;
+
+    @Size(min = 1, max = 100, message = "surname must be between 1 and 100 characters")
+    @NotBlank(message = "surname can not be blank")
     private String surname;
+
+    @NotNull(message = "isFirm can not be null")
+    private Boolean isFirm;
 }
