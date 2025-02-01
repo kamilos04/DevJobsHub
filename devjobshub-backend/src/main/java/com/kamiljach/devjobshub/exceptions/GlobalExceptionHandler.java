a4e02dd22a51cb1f86b1301f14274eb9080082be
Kamil Jach <kamiljach2004@gmail.com>
2025-02-01 12:18:20 +0100

    fix-backend: exceptions small fix

--- devjobshub-backend/src/main/java/com/kamiljach/devjobshub/exceptions/GlobalExceptionHandler.java
+++ devjobshub-backend/src/main/java/com/kamiljach/devjobshub/exceptions/GlobalExceptionHandler.java
@@ -26,97 +26,97 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccountAlreadyExistsException.class)
     public ResponseEntity<ApiError> handleAccountAlreadyExistsException(AccountAlreadyExistsException ex) {
-        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "Account already exists");
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage());
         return new ResponseEntity<ApiError>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(UserNotFoundByJwtException.class)
     public ResponseEntity<ApiError> handleInvalidJwtExceptionException(UserNotFoundByJwtException ex) {
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "User not found, invalid jwt");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<ApiError>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(OfferNotFoundByIdException.class)
     public ResponseEntity<ApiError> handleOfferNotFoundByIdException(OfferNotFoundByIdException ex) {
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Offer not found, invalid ID");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<ApiError>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(TechnologyWithThisNameAlreadyExistsException.class)
     public ResponseEntity<ApiError> handleTechnologyWithThisNameAlreadyExistsException(TechnologyWithThisNameAlreadyExistsException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Technology with this name already exists");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<ApiError>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(TechnologyNotFoundByNameException.class)
     public ResponseEntity<ApiError> handleTechnologyNotFoundByNameException(TechnologyNotFoundByNameException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Technology not found by name, invalid name");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<ApiError>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(TechnologyNotFoundByIdException.class)
     public ResponseEntity<ApiError> handleTechnologyNotFoundByIdException(TechnologyNotFoundByIdException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Technology not found by ID, invalid ID");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<ApiError>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(ApplicationNotFoundByIdException.class)
     public ResponseEntity<ApiError> handleApplicationNotFoundByIdException(ApplicationNotFoundByIdException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Application not found, invalid ID");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<ApiError>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(QuestionOrAnswerIsIncorrectException.class)
     public ResponseEntity<ApiError> handleQuestionOrAnswerIsIncorrectException(QuestionOrAnswerIsIncorrectException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Question or answer is incorrect");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(OfferIsAlreadyLikedByUserException.class)
     public ResponseEntity<ApiError> handleOfferIsAlreadyLikedByUserException(OfferIsAlreadyLikedByUserException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Offer is already liked by user");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(OfferIsNotLikedByUserException.class)
     public ResponseEntity<ApiError> handleOfferIsNotLikedByUserException(OfferIsNotLikedByUserException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Offer is not liked by user");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(ApplicationAlreadyIsInFavouritesException.class)
     public ResponseEntity<ApiError> handleApplicationAlreadyIsInFavouritesException(ApplicationAlreadyIsInFavouritesException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Application already is in favourites");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(ApplicationIsNotInFavouritesException.class)
     public ResponseEntity<ApiError> handleApplicationIsNotInFavouritesException(ApplicationIsNotInFavouritesException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Application is not in favourites");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(OfferExpiredException.class)
     public ResponseEntity<ApiError> handleOfferExpiredException(OfferExpiredException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "Offer has already expired");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(UserNotFoundByIdException.class)
     public ResponseEntity<ApiError> handleUserNotFoundByIdException(UserNotFoundByIdException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "User not found, invalid ID");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(UserIsAlreadyRecruiterException.class)
     public ResponseEntity<ApiError> handleUserIsAlreadyRecruiterException(UserIsAlreadyRecruiterException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "User is already a recruiter");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
     @ExceptionHandler(UserIsNotRecruiterException.class)
     public ResponseEntity<ApiError> handleUserIsNotRecruiterException(UserIsNotRecruiterException ex){
-        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, "User is not a recruiter");
+        ApiError apiError = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 
