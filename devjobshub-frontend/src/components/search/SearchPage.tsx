2fdfc757f9b80ee5ddbf78a1cdcfade1d449dcc2
Kamil Jach <kamiljach2004@gmail.com>
2025-02-08 15:51:30 +0100

    wip-frontend: search page - only changing page number doesn't work

--- devjobshub-frontend/src/components/search/SearchPage.tsx
+++ devjobshub-frontend/src/components/search/SearchPage.tsx
@@ -33,7 +33,10 @@ import {
 } from "@/components/ui/pagination"
 import OfferCard from './OfferCard'
 import { Offer } from '@/types/offer'
-import { useNavigate } from 'react-router'
+import { useNavigate, useSearchParams } from 'react-router'
+import { getTechnologiesByIds } from '@/state/technology/action'
+import { text } from 'stream/consumers'
+
 
 const SearchPage = () => {
 
@@ -41,16 +44,37 @@ const SearchPage = () => {
   const [technologies, setTechnologies] = React.useState<Array<Technology>>([])
   const dispatch = useDispatch<any>()
   const storeOffer = useSelector((store: any) => store.offer)
+  const storeTechnology = useSelector((store: any) => store.technology)
   const [localizationText, setLocalizationText] = React.useState<string>("")
   const [localizations, setLocalizations] = React.useState<Array<string>>([])
   const [pageNumber, setPageNumber] = React.useState<number>(0)
   const navigate = useNavigate()
+  const [searchParams, setSearchParams] = useSearchParams();
+  
+  
+  
+  const {
+    register: registerSearch,
+    handleSubmit: handleSubmit,
+    control: controlSearch,
+    formState: { errors: searchErrors },
+    setValue: setValueSearch,
+    watch: watchSearch
+  } = useForm({
+    // resolver: yupResolver(createOfferSchema)
+    // defaultValues: {sortBy: "dateTimeOfCreation", sortingDirection: "asc"}
 
-  const handleSubmitSearch = (data: any) => {
-    const specializationsString = specializations.filter(element => data[element]).join(",")
-    const operatingModesString = operatingModes.filter(element => data[element]).join(",")
-    const jobLevelsString = jobLevels.filter(element => data[element]).join(",")
-    const text = data.text
+  })
+
+  // const formValues = watchSearch();
+
+  const handleSubmitSearch = () => {
+    const searchValues = watchSearch();
+    // console.log(formValues.sortBy)
+    const specializationsString = specializations.filter(element => searchValues[element]).join(",")
+    const operatingModesString = operatingModes.filter(element => searchValues[element]).join(",")
+    const jobLevelsString = jobLevels.filter(element => searchValues[element]).join(",")
+    const text = searchValues.text
 
     let params = ""
     if (specializationsString) {
@@ -73,40 +97,185 @@ const SearchPage = () => {
     }
     params += "numberOfElements=10&"
     params += `pageNumber=${pageNumber}&`
-    params += "sortBy=dateTimeOfCreation&"
-    params += "sortingDirection=ASC&"
+    params += `sortBy=${searchValues.sortBy}&`
+    params += `sortingDirection=${searchValues.sortingDirection}&`
     console.log(params)
     dispatch(searchOffers(params))
   }
 
 
+  const handleCreateNewUrl = (data: any) => {
+    // console.log(data)
+    const paramsURL = new URLSearchParams()
 
-  const {
-    register: registerSearch,
-    handleSubmit: handleSearch,
-    control: controlSearch,
-    formState: { errors: searchErrors },
-    watch: watchSearch
-  } = useForm({
-    // resolver: yupResolver(createOfferSchema)
-  })
+    const specializationsString = specializations.filter(element => data[element]).join(",")
+    const operatingModesString = operatingModes.filter(element => data[element]).join(",")
+    const jobLevelsString = jobLevels.filter(element => data[element]).join(",")
+    const text = data.text
+
+    if (specializationsString) {
+      paramsURL.set("specializations", specializationsString)
+    }
+    if (operatingModesString) {
+      paramsURL.set("operatingModes", operatingModesString)
+    }
+    if (jobLevelsString) {
+      paramsURL.set("jobLevels", jobLevelsString)
+    }
+    if (text) {
+      paramsURL.set("text", text)
+    }
+    if (technologies.length !== 0) {
+      const technologiesIdsString = technologies.map(t => t.id).join(",")
+      paramsURL.set("technologies", technologiesIdsString)
+    }
+    if (localizations.length !== 0) {
+      const localizationsString = localizations.map(l => l).join(",")
+      paramsURL.set("localizations", localizationsString)
+    }
+    paramsURL.set("pageNumber", pageNumber.toString())
+    paramsURL.set("sortBy", data.sortBy)
+    paramsURL.set("sortingDirection", data.sortingDirection)
+    setSearchParams(paramsURL)
+  }
+
+
+  
+
+
+
+  // const defaultValues = {
+  //   text: searchParams.get("text") || "",xxxx
+  //   specializations: searchParams.get("specializations")?.split(",") || [],xxx
+  //   operatingModes: searchParams.get("operatingModes")?.split(",") || [],xxx
+  //   jobLevels: searchParams.get("jobLevels")?.split(",") || [],xxx
+  //   sortBy: searchParams.get("sortBy") || "dateTimeOfCreation",xxxx
+  //   sortingDirection: searchParams.get("sortingDirection") || "asc",xxx
+  //   pageNumber: Number(searchParams.get("pageNumber")) || 0,xxxx
+  //   localizations: searchParams.get("localizations") || []xxxx
+  // };
 
   // const data = watchSearch()
-
+  
   const specializations = ["BACKEND", "FRONTEND", "FULLSTACK", "MOBILE", "EMBEDDED", "ARCHITECTURE", "TESTING", "SECURITY", "AIML", "DATASCIENCE", "HELPDESK", "UXUI", "ADMINISTRATOR", "PROJECTMANAGER", "OTHER"]
   const operatingModes = ["REMOTE", "STATIONARY", "HYBRID"]
   const jobLevels = ["TRAINEE", "JUNIOR", "MID", "SENIOR", "MANAGER", "CLEVEL"]
 
+  // useEffect(() =>{
+  //   console.log(formValues.sortBy)
+  //   console.log("zmieniono")
+  // }, [formValues.sortBy])
+
+
+  // useEffect(() => {
+  //   handleSearch(handleSubmitSearch)()
+  // }, [pageNumber, formValues.sortingDirection, formValues.sortBy])
 
   useEffect(() => {
-    handleSearch(handleSubmitSearch)()
-  }, [pageNumber])
+    if(searchParams.get("sortBy")===null || searchParams.get("sortingDirection")===null || searchParams.get("pageNumber")===null){
+      console.log('PUSE')
+      handleSubmit(handleCreateNewUrl)()
+    }
+    console.log("ustawiam")
+
+    if(searchParams.get("text")){
+      setValueSearch("text", searchParams.get("text"))
+    }
+    else{
+      setValueSearch("text", "")
+    }
+
+    if(searchParams.get("sortBy")){
+      setValueSearch("sortBy", searchParams.get("sortBy"))
+    }
+    else{
+      setValueSearch("sortBy", "dateTimeOfCreation")
+    }
+
+    if(searchParams.get("sortingDirection")){
+      setValueSearch("sortingDirection", searchParams.get("sortingDirection"))
+    }
+    else{
+      setValueSearch("sortingDirection", "asc")
+    }
+
+    if(searchParams.get("pageNumber")){
+      setPageNumber(Number(searchParams.get("pageNumber")))
+    }
+    else{
+      setPageNumber(0)
+    }
+
+    if(searchParams.get("localizations")){
+      setLocalizations(searchParams.get("localizations")?.split(",") || [])
+    }
+    else{
+      setLocalizations([])
+    }
+
+    const specializationsList = searchParams.get("specializations")?.split(",") || []
+    specializations.forEach((element) => {
+      if(specializationsList.includes(element)){
+        setValueSearch(element, true)
+      }
+      else{
+        setValueSearch(element, false)
+      }
+    })
+
+    const operatingModesList = searchParams.get("operatingModes")?.split(",") || []
+    operatingModes.forEach((element) => {
+      if(operatingModesList.includes(element)){
+        setValueSearch(element, true)
+      }
+      else{
+        setValueSearch(element, false)
+      }
+    })
+
+    const jobLevelsList = searchParams.get("jobLevels")?.split(",") || []
+    jobLevels.forEach((element) => {
+      if(jobLevelsList.includes(element)){
+        setValueSearch(element, true)
+      }
+      else{
+        setValueSearch(element, false)
+      }
+    })
+
+    if(searchParams.get("technologies")){
+      const technologiesIdsString = searchParams.get("technologies") || ""
+
+      dispatch(getTechnologiesByIds(technologiesIdsString))
+    }
+    else{
+      setTechnologies([])
+      handleSubmitSearch()
+    }
+    
+
+    
+  }, [searchParams])
+
+
+
+  useEffect(() => {
+    if(storeTechnology.success==="getTechnologiesByIds"){
+      setTechnologies(storeTechnology.technologiesByIds)
+      handleSubmitSearch()
+    }
+  }, [storeTechnology.success])
+
+
+  // useEffect(() => {
+  //   handleSubmit(handleCreateNewUrl)()
+  // }, [pageNumber])
 
   return (
     <div className='flex flex-col'>
       <Navbar />
       <div className='flex flex-col items-center'>
-        <form onSubmit={handleSearch(handleSubmitSearch)}>
+        {/* <form onSubmit={handleSearch(handleSubmitSearch)}> */}
           <div className='mt-8 p-4 w-[90rem] flex flex-col items-center rounded-2xl'>
             <div className='bg-my-card flex flex-col p-4 rounded-xl border-[1px] w-full'>
               <div className='flex flex-row justify-start mb-2'>
@@ -114,7 +283,7 @@ const SearchPage = () => {
               </div>
 
               <div className='flex flex-row'>
-                <Input type="text" placeholder="Search" className='h-12 rounded-none' {...registerSearch('text')}/>
+                <Input type="text" placeholder="Search" className='h-12 rounded-none' {...registerSearch('text')} onKeyDown={(event) => {if (event.key === "Enter") {handleSubmit(handleCreateNewUrl)()}}}/>
 
                 <PopoverCheckboxes text="Specializations" control={controlSearch} checkboxes={[
                   { registerAs: "BACKEND", label: "Backend" },
@@ -142,7 +311,7 @@ const SearchPage = () => {
                   { registerAs: "MANAGER", label: "Manager" },
                   { registerAs: "CLEVEL", label: "C-level" },
                 ]} />
-                <Button type='submit' className='h-12 ml-6 rounded-2xl'><div className='flex flex-row items-center space-x-2'><FaSearch /> <span className='text-sm'>Search</span></div></Button>
+                <Button type='submit' className='h-12 ml-6 rounded-2xl'><div className='flex flex-row items-center space-x-2' onClick={handleSubmit(handleCreateNewUrl)}><FaSearch /> <span className='text-sm'>Search</span></div></Button>
               </div>
 
               {/* {storeOffer.searchOffers?.content?.map((element: any) => <span>{element.id}</span>)} */}
@@ -185,14 +354,14 @@ const SearchPage = () => {
 
 
 
-
+                <Button className='mt-4' variant={'secondary'} onClick={handleSubmit(handleCreateNewUrl)}>Filter</Button>
               </div>
               <div className='ml-4 flex-col w-full'>
-                <div className='flex flex-row space-x-4'>
+                <div className='flex flex-row space-x-4 items-center'>
                   <Controller
                     name="sortBy"
                     control={controlSearch}
-                    defaultValue={"dateTimeOfCreation"}
+                    // defaultValue={"dateTimeOfCreation"}
 
                     render={({ field }) => (
                       <>
@@ -214,25 +383,25 @@ const SearchPage = () => {
                   <Controller
                     name="sortingDirection"
                     control={controlSearch}
-                    defaultValue={"ASC"}
+                    // defaultValue={"asc"}
 
                     render={({ field }) => (
                       <>
                         <Select value={field.value} onValueChange={field.onChange}>
                           <SelectTrigger className="w-auto">
-                            Sorting direction: {field.value === "ASC" ? "Ascending" : "Descending"}
+                            Sorting direction: {field.value === "asc" ? "Ascending" : "Descending"}
                           </SelectTrigger>
                           <SelectContent>
                             <SelectGroup>
-                              <SelectItem value="ASC">Ascending</SelectItem>
-                              <SelectItem value="DSC">Descending</SelectItem>
+                              <SelectItem value="asc">Ascending</SelectItem>
+                              <SelectItem value="dsc">Descending</SelectItem>
                             </SelectGroup>
                           </SelectContent>
                         </Select>
                       </>
                     )}
                   />
-
+                  {storeOffer.searchOffers && <span>{storeOffer.searchOffers?.totalElements} offers found</span>}
 
 
                 </div>
@@ -286,7 +455,7 @@ const SearchPage = () => {
               </PaginationContent>
             </Pagination>
           </div>
-        </form>
+        {/* </form> */}
       </div>
 
     </div>
