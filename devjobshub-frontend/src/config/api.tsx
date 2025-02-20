e0466b901a0ad0d25f61f2f5059c17b02f1403e2
Kamil Jach <kamiljach2004@gmail.com>
2025-02-20 19:09:21 +0100

    chore: gitignore

--- devjobshub-frontend/src/config/api.tsx
+++ devjobshub-frontend/src/config/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = "http://157.158.191.53:8080"
+export const API_URL = "http://192.168.1.13:8080"
 
 export const api = axios.create({
     baseURL: API_URL,
